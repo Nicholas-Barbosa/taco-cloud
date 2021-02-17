ce0a12adfa1ba64b5d18242324f6baa3d6ab0ef3
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-17 17:22:16 -0300

    Spring MVC Controller test

---
+++ src/test/java/tacos/controller/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package tacos.controller;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+class HomeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void test() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("home"));
+	}
+
+}
