ea8c30ddb24bea2cb5301d4b59c7ee2fa1cd35cd
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-17 17:14:06 -0300

    Spring MVC Controller class

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -3,14 +3,13 @@ package tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootConfiguration
-//@EnableAutoConfiguration
-//@ComponentScan
+
 @SpringBootApplication
 public class TacoCloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
+
 
 }
