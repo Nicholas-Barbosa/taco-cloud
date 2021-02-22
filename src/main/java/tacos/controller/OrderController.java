fd517c645f258e40ac5b696d9bed953376538a8e
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-22 12:11:53 -0300

    Added method handling for Order HTTP Post

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -5,7 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import tacos.dto.OrderForm;
 
 @Controller
 @RequestMapping("/orders")
@@ -15,6 +18,13 @@ public class OrderController {
 
 	@GetMapping("/current")
 	public String orderForm(Model model) {
+		model.addAttribute("order", new OrderForm());
 		return "orderForm";
+	}
+
+	@PostMapping
+	public String processOrder(OrderForm order) {
+		log.info("Order submitted: " + order);
+		return "redirect:/";
 	}
 }
