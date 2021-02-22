d947c94853a4c2d043608d3c64e3673f1909527f
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-22 12:16:02 -0300

    Created OrderForm DTO class

--- src/main/java/tacos/dto/OrderForm.java
+++ src/main/java/tacos/dto/OrderForm.java
@@ -2,4 +2,84 @@ package tacos.dto;
 
 public class OrderForm {
 
+	private String name;
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	private String ccNumber;
+	private String ccExpiration;
+	private String ccCVV;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getCcNumber() {
+		return ccNumber;
+	}
+
+	public void setCcNumber(String ccNumber) {
+		this.ccNumber = ccNumber;
+	}
+
+	public String getCcExpiration() {
+		return ccExpiration;
+	}
+
+	public void setCcExpiration(String ccExpiration) {
+		this.ccExpiration = ccExpiration;
+	}
+
+	public String getCcCVV() {
+		return ccCVV;
+	}
+
+	public void setCcCVV(String ccCVV) {
+		this.ccCVV = ccCVV;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderForm [name=" + name + ", street=" + street + ", city=" + city + ", state=" + state + ", zip=" + zip
+				+ ", ccNumber=" + ccNumber + ", ccExpiration=" + ccExpiration + ", ccCVV=" + ccCVV + "]";
+	}
+
+	
 }
