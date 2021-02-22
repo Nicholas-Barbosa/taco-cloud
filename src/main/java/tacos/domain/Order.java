1f1e23187f24fdcc8ddeb49aaab8cb54fac41de0
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-22 16:17:30 -0300

    Validation thymeleaf render components following errors

--- src/main/java/tacos/domain/Order.java
+++ src/main/java/tacos/domain/Order.java
@@ -20,9 +20,9 @@ public class Order {
 	private String zip;
 	@CreditCardNumber(message = "Not a valid credit card number!")
 	private String ccNumber;
-	@Pattern(regexp = "^(0[1-9]|1[0-2]) ([\\/]) ([1-9][0-9])$")
+	@Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message = "Must be formatted MM/YY")
 	private String ccExpiration;
-	@Digits(integer = 3,fraction = 0,message = "Invalid CVV")
+	@Digits(integer = 3, fraction = 0, message = "Invalid CVV")
 	private String ccCVV;
 
 	public String getName() {
