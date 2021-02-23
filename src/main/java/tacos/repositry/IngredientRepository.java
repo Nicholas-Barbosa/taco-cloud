47e426843c1f4704489794d5674560e58efa7b43
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-23 09:25:14 -0300

    Create IngredientRepository.java

---
+++ src/main/java/tacos/repositry/IngredientRepository.java
@@ -0,0 +1,14 @@
+package tacos.repositry;
+
+import java.util.Optional;
+
+import tacos.domain.Ingredient;
+
+public interface IngredientRepository {
+
+	Iterable<Ingredient> findAll();
+
+	Optional<Ingredient> findById(String id);
+
+	Ingredient save(Ingredient ingredient);
+}
