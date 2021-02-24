3ff79daaa93e7183b5e8cef28273d2864f60e1a7
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-24 10:01:57 -0300

    Added User entity

---
+++ src/main/java/tacos/domain/User.java
@@ -0,0 +1,124 @@
+package tacos.domain;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class User implements UserDetails {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private final Long id;
+
+	private final String username;
+	private final String password;
+	private final String fullname;
+	private final String street;
+	private final String city;
+	private final String state;
+	private final String zip;
+	private final String phoneNumber;
+
+	public User() {
+		this(null, null, null, null, null, null, null, null, null);
+	}
+
+	public User(Long id, String username, String password, String fullname, String street, String city, String state,
+			String zip, String phoneNumber) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.fullname = fullname;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,23 +1,13 @@
 package tacos.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	private final DataSource dataSource;
-
-	public SecurityConfig(DataSource dataSource) {
-		super();
-		this.dataSource = dataSource;
-	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
