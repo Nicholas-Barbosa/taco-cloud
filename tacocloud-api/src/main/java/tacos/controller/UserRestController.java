9ff81e1bf1ba1ee108d238e090d834b34dc856d3
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-02-26 10:35:17 -0300

    Added CollectionModel, WebMvcLinkBuilder to return and build link object

--- tacocloud-api/src/main/java/tacos/controller/UserRestController.java
+++ tacocloud-api/src/main/java/tacos/controller/UserRestController.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-import javax.servlet.http.HttpServletRequest;
-
-
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +33,22 @@ public class UserRestController {
 		Consumer<User> c = u -> users.add(UserDTO.toDTO(u));
 		userCrudService.findAll().forEach(c);
 		return users;
+	}
+
+	@GetMapping("/links")
+	public CollectionModel<EntityModel<UserDTO>> showUsersWithLinks() {
+		/*
+		 * Resources now is CollectionModel
+		 * 
+		 * Resource now is EntityModel
+		 */
+		List<UserDTO> users = new ArrayList<>();
+		Consumer<User> c = u -> users.add(UserDTO.toDTO(u));
+		userCrudService.findAll().forEach(c);
+
+		CollectionModel<EntityModel<UserDTO>> resources = CollectionModel.wrap(users);
+
+		resources.add(WebMvcLinkBuilder.linkTo(UserRestController.class).slash("/links").withRel("users"));
+		return resources;
 	}
 }
