d6a889f1b0f6da17cfbe6cd9691dca38673b285c
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-03-02 11:21:46 -0300

    Create MessageConverterCovariantCfg.java
    Using Jackson2JsonMessageConverter as covariant of MessageConverter

---
+++ tacocloud-messaging-rabbitmq/src/main/java/tacos/MessageConverterCovariantCfg.java
@@ -0,0 +1,15 @@
+package tacos;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageConverterCovariantCfg {
+
+	@Bean
+	public MessageConverter converter() {
+		return new Jackson2JsonMessageConverter();
+	}
+}
