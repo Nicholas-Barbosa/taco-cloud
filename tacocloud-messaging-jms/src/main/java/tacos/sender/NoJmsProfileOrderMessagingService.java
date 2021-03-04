e11167508811a52410c41cf92918f87159c1dbc6
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-03-03 21:51:33 -0300

    Added profiles for managing jms ,etc

---
+++ tacocloud-messaging-jms/src/main/java/tacos/sender/NoJmsProfileOrderMessagingService.java
@@ -0,0 +1,19 @@
+package tacos.sender;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import tacos.messages.OrderJmsMessgae;
+
+@Service
+public class NoJmsProfileOrderMessagingService implements OrderMessagingService {
+
+	
+
+	@Override
+	public void senOrder(OrderJmsMessgae order) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
