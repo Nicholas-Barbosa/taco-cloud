145a698f48a088e39b8123903a41388e055a6bd9
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-03-04 21:18:43 -0300

    Getting started with Streams in Project Reactor
    Very similar to java streams api

--- reactor-module/src/test/java/tacos/test/TestReactor.java
+++ reactor-module/src/test/java/tacos/test/TestReactor.java
@@ -15,122 +15,122 @@ import reactor.test.StepVerifier;
 
 class TestReactor {
 
-//	@Test
-//	void test() {
-//
-//		Flux<String> publisher = Flux.just("n", "i");
-//		publisher.map(x -> {
-//			System.out.println("x " + x);
-//			return x;
-//		}).subscribe(System.out::println);
-//
-//		System.out.println("---Java Stream---");
-//		Stream<String> stream = Stream.of("n", "i");
-//		stream.map(x -> {
-//			System.out.println("x " + x);
-//			return x;
-//		}).forEach(System.out::println);
-//	}
-//
-//	@Test
-//	void createFluxFromArray() {
-//		Flux<String> flux = Flux.fromArray(new String[] { "Apple", "Banana" });
-//		Assert.assertEquals(2, flux.count().block().longValue());
-//	}
-//
-//	@Test
-//	void createFluxFromIterable() {
-//		Flux<String> flux = Flux.fromIterable(List.of("Apple", "Banana"));
-//		Assert.assertEquals(2, flux.count().block().longValue());
-//	}
-//
-//	@Test
-//	void createFluxFromStream() {
-//		Flux<String> flux = Flux.fromStream(Stream.of("Apple", "Banana"));
-//		Assert.assertEquals(2, flux.count().block().longValue());
-//	}
-//
-//	@Test
-//	void generatingFlux() {
-//		Flux<Integer> flux = Flux.range(0, 101);
-//		Assert.assertEquals(101, flux.count().block().longValue());
-//	}
-//
-//	@Test
-//	void intervalFlux() {
-//		Flux<Long> flux = Flux.interval(Duration.ofSeconds(1)).take(5);
-//		Assert.assertEquals(5, flux.count().block().longValue());
-//	}
-//
-//	@Test
-//	void mergeFlux() {
-//		System.out.println("---merging---");
-//
-//		Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa");
-//		Flux<String> foodFlux = Flux.just("Lasagna", "Lollipops", "Apples");
-//
-//		characterFlux.mergeWith(foodFlux).subscribe(System.out::println);
-//		System.out.println("---end merging---");
-//	}
-//
-//	@Test
-//	void tuple() {
-//		System.out.println("---zipping---");
-//		/*
-//		 * Reduction para pares de elementos de fontes diferentes.
-//		 */
-//		Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa");
-//		Flux<String> foodFlux = Flux.just("Lasagna", "Lollipops", "Apples");
-//
-//		Flux.zip(characterFlux, foodFlux, (c, f) -> c + " eat " + f).subscribe(System.out::println);
-//		System.out.println("---end zipping---");
-//	}
-//
-//	@Test
-//	void first() {
-//		System.out.println("---first---");
-//		/*
-//		 * Reduction para pares de elementos de fontes diferentes.
-//		 */
-//		Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa")
-//				.delaySubscription(Duration.ofMillis(250));
-//		Flux<String> foodFlux = Flux.just("Lasagna", "Lollipops", "Apples");
-//
-//		StepVerifier.create(Flux.firstWithSignal(characterFlux, foodFlux)).expectNext("Lasagna").expectNext("Lollipops")
-//				.expectNext("Apples").verifyComplete();
-//		System.out.println("---end first---");
-//
-//	}
-//
-//	@Test
-//	void skipAFewSeconds() {
-//		Flux<String> fluxToSkip = Flux.just("a", "b", "c", "d", "s");
-//		StepVerifier.create(fluxToSkip.delayElements(Duration.ofSeconds(1)).skip(Duration.ofSeconds(4)))
-//				.expectNext("d", "s").verifyComplete();
-//	}
-//
-//	@Test
-//	void testeTake() {
-//		Flux<String> fluxToSkip = Flux.just("a", "b", "c", "d", "s");
-//		StepVerifier.create(fluxToSkip.take(3)).expectNext("a", "b", "c").expectComplete();
-//
-//		StepVerifier.create(fluxToSkip.delayElements(Duration.ofSeconds(1)).take(Duration.ofMillis(1200)))
-//				.expectNext("a").verifyComplete();
-//		
-//		StepVerifier.create(fluxToSkip.delaySubscription(Duration.ofSeconds(1)).take(Duration.ofMillis(1200)))
-//		.expectNext("a","b", "c", "d", "s").verifyComplete();
-//	}
-//
-//	@Test
-//	void testFilter() {
-//		Flux<Integer> fluxToSkip = Flux.range(0, 1000);
-//		StepVerifier.create(fluxToSkip.filter(x -> x <= 5)).expectNext(0, 1, 2, 3, 4, 5).verifyComplete();
-//	}
-	
+	@Test
+	void test() {
+
+		Flux<String> publisher = Flux.just("n", "i");
+		publisher.map(x -> {
+			System.out.println("x " + x);
+			return x;
+		}).subscribe(System.out::println);
+
+		System.out.println("---Java Stream---");
+		Stream<String> stream = Stream.of("n", "i");
+		stream.map(x -> {
+			System.out.println("x " + x);
+			return x;
+		}).forEach(System.out::println);
+	}
+
+	@Test
+	void createFluxFromArray() {
+		Flux<String> flux = Flux.fromArray(new String[] { "Apple", "Banana" });
+		Assert.assertEquals(2, flux.count().block().longValue());
+	}
+
+	@Test
+	void createFluxFromIterable() {
+		Flux<String> flux = Flux.fromIterable(List.of("Apple", "Banana"));
+		Assert.assertEquals(2, flux.count().block().longValue());
+	}
+
+	@Test
+	void createFluxFromStream() {
+		Flux<String> flux = Flux.fromStream(Stream.of("Apple", "Banana"));
+		Assert.assertEquals(2, flux.count().block().longValue());
+	}
+
+	@Test
+	void generatingFlux() {
+		Flux<Integer> flux = Flux.range(0, 101);
+		Assert.assertEquals(101, flux.count().block().longValue());
+	}
+
+	@Test
+	void intervalFlux() {
+		Flux<Long> flux = Flux.interval(Duration.ofSeconds(1)).take(5);
+		Assert.assertEquals(5, flux.count().block().longValue());
+	}
+
+	@Test
+	void mergeFlux() {
+		System.out.println("---merging---");
+
+		Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa");
+		Flux<String> foodFlux = Flux.just("Lasagna", "Lollipops", "Apples");
+
+		characterFlux.mergeWith(foodFlux).subscribe(System.out::println);
+		System.out.println("---end merging---");
+	}
+
+	@Test
+	void tuple() {
+		System.out.println("---zipping---");
+		/*
+		 * Reduction para pares de elementos de fontes diferentes.
+		 */
+		Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa");
+		Flux<String> foodFlux = Flux.just("Lasagna", "Lollipops", "Apples");
+
+		Flux.zip(characterFlux, foodFlux, (c, f) -> c + " eat " + f).subscribe(System.out::println);
+		System.out.println("---end zipping---");
+	}
+
+	@Test
+	void first() {
+		System.out.println("---first---");
+		/*
+		 * Reduction para pares de elementos de fontes diferentes.
+		 */
+		Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa")
+				.delaySubscription(Duration.ofMillis(250));
+		Flux<String> foodFlux = Flux.just("Lasagna", "Lollipops", "Apples");
+
+		StepVerifier.create(Flux.firstWithSignal(characterFlux, foodFlux)).expectNext("Lasagna").expectNext("Lollipops")
+				.expectNext("Apples").verifyComplete();
+		System.out.println("---end first---");
+
+	}
+
+	@Test
+	void skipAFewSeconds() {
+		Flux<String> fluxToSkip = Flux.just("a", "b", "c", "d", "s");
+		StepVerifier.create(fluxToSkip.delayElements(Duration.ofSeconds(1)).skip(Duration.ofSeconds(4)))
+				.expectNext("d", "s").verifyComplete();
+	}
+
+	@Test
+	void testeTake() {
+		Flux<String> fluxToSkip = Flux.just("a", "b", "c", "d", "s");
+		StepVerifier.create(fluxToSkip.take(3)).expectNext("a", "b", "c").expectComplete();
+
+		StepVerifier.create(fluxToSkip.delayElements(Duration.ofSeconds(1)).take(Duration.ofMillis(1200)))
+				.expectNext("a").verifyComplete();
+
+		StepVerifier.create(fluxToSkip.delaySubscription(Duration.ofSeconds(1)).take(Duration.ofMillis(1200)))
+				.expectNext("a", "b", "c", "d", "s").verifyComplete();
+	}
+
+	@Test
+	void testFilter() {
+		Flux<Integer> fluxToSkip = Flux.range(0, 1000);
+		StepVerifier.create(fluxToSkip.filter(x -> x <= 5)).expectNext(0, 1, 2, 3, 4, 5).verifyComplete();
+	}
+
 	@Test
 	void testFlatMap() {
 		Flux<Integer> flux = Flux.just(1, 2, 3);
-		flux. flatMap(x -> {
+		flux.flatMap(x -> {
 			try {
 				System.out.println("Nicholas Thread from pReactor sleeping...");
 				Thread.sleep(1000);
@@ -157,5 +157,27 @@ class TestReactor {
 			}
 			return Stream.of(x * 10);
 		}).collect(Collectors.counting());
+	}
+
+	@Test
+	void testBufferFlux() {
+		Flux<String> flux = Flux.just("data1", "data2", "data3", "data4");
+		flux.buffer(2).log().subscribe(System.out::println);
+		StepVerifier.create(flux.buffer(2)).expectNext(List.of("data1", "data2"), List.of("data3", "data4"))
+				.expectComplete();
+	}
+
+	@Test
+	void collectMap() {
+		// Like Streams
+		Flux<String> flux = Flux.just("data1", "data2", "data3", "data4");
+		flux.collectMap(x -> "Key:" + x + "K", v -> "value: " + v).subscribe(System.out::println);
+	}
+
+	@Test
+	void terminalLogicOperators() {
+		Flux<Integer> flux = Flux.just(10, 20, 30);
+		flux.all(x -> x >= 10).log().subscribe();
+		StepVerifier.create(flux.any(x -> x == 10)).expectNext(true).verifyComplete();
 	}
 }
