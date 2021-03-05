a17451370404cab6f9773a2a1aba27d50367a1d5
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-03-05 10:20:36 -0300

    Execution between reactive stream pReactor and java stream

---
+++ reactor-module/src/test/java/tacos/test/ParallelStreamsTest.java
@@ -0,0 +1,63 @@
+package tacos.test;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+class ParallelStreamsTest {
+
+	public static void main(String[] args) {
+
+	}
+
+	@Test
+	void test() throws InterruptedException {
+		Flux<Integer> flux = Flux.just(1, 2, 3);
+		flux.parallel().runOn(Schedulers.parallel()).log().map(x -> {
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return x * 10;
+		}).subscribe(x -> {
+			System.out.println("Element " + x);
+		});
+
+	}
+
+	@Test
+	void testStream() throws InterruptedException {
+		Stream<Integer> stream = Stream.of(1, 2, 3);
+		ExecutorService executor = null;
+		try {
+			executor = Executors.newCachedThreadPool();
+			executor.execute(() -> {
+				stream.parallel().map(x -> {
+					try {
+						Thread.sleep(1000);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					return x * 10;
+				}).map(x -> {
+					System.out.println("Element " + x);
+					return x;
+				}).collect(Collectors.counting());
+			});
+		}finally {
+			executor.shutdown();
+		}
+		
+
+	}
+}
