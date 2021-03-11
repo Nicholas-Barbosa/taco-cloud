eaf496f46dd09d9b1e1dc7f222d01638efc97cba
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-03-11 15:31:27 -0300

    Testing configurations from ms

--- tacocloud-ingredient-client/src/main/java/tacos/controller/PropertiesController.java
+++ tacocloud-ingredient-client/src/main/java/tacos/controller/PropertiesController.java
@@ -1,5 +1,19 @@
 package tacos.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/properties")
 public class PropertiesController {
 
+	@Value("${user}")
+	private String user;
+
+	@GetMapping
+	public String showUser() {
+		return user;
+	}
 }
