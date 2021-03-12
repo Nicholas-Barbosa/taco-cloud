d8fcd41521e580b90a5950610fed070c52cf736c
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-03-12 15:29:35 -0300

    Update IngredientController.java
    time out latency

--- tacocloud-ingredient-service/src/main/java/ingredients/controller/IngredientController.java
+++ tacocloud-ingredient-service/src/main/java/ingredients/controller/IngredientController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 import ingredients.dto.IngredientDTO;
 
@@ -20,14 +21,23 @@ public class IngredientController {
 				new IngredientDTO("chicken2", IngredientDTO.Type.PROTEIN),
 				new IngredientDTO("chicken", IngredientDTO.Type.PROTEIN));
 	}
-	
+
 	@GetMapping("/throw-exception")
 	@HystrixCommand(fallbackMethod = "showIngredientsFallBack")
 	public Iterable<IngredientDTO> showIngredientsException() {
-		
+
 		throw new RuntimeException("Testing circuit braker!");
 	}
-	
+
+	@GetMapping("/timeout")
+	@HystrixCommand(fallbackMethod = "showIngredientsFallBack", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500") })
+	public Iterable<IngredientDTO> showIngredientsTimeout() throws Exception {
+
+		Thread.sleep(1000);
+		return null;
+	}
+
 	private Iterable<IngredientDTO> showIngredientsFallBack() {
 		return Arrays.asList(new IngredientDTO("falBack", IngredientDTO.Type.PROTEIN),
 				new IngredientDTO("falBack2", IngredientDTO.Type.PROTEIN),
