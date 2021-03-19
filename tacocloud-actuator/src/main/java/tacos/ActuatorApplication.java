7db07acbea855f926888ed4e11989b00246e4164
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-03-19 12:17:08 -0300

    added httptrace in memory

--- tacocloud-actuator/src/main/java/tacos/ActuatorApplication.java
+++ tacocloud-actuator/src/main/java/tacos/ActuatorApplication.java
@@ -2,6 +2,7 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
 public class ActuatorApplication {
