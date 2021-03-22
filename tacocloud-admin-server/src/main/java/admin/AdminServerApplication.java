39d2679d275a3ead559976ff78669ff43fbd5de7
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-03-22 10:46:44 -0300

    Added admin server to consume actuator endpoints

---
+++ tacocloud-admin-server/src/main/java/admin/AdminServerApplication.java
@@ -0,0 +1,15 @@
+package admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerApplication.class, args);
+	}
+}
